6ec06e88badf0fcfa7444cf04959429d27758ce2
Ragne Samson <ragnesamson@MacBook-Pro.lan>
2021-02-04 15:46:01 +0200

    Add Products.vue new file

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,6 +1,12 @@
 <template>
 <div>
     <h2>Tooted</h2>
+    <b-table striped hover :items="productItems" :fields="productFields">
+         <template #cell(price)="data">
+        <b class="text-info">{{ data.value}} EUR</b>
+         </template>
+    </b-table>
+
         </div>
     
 </template>
@@ -8,16 +14,47 @@
 <script>
 import axios from 'axios';
 export default {
+    name:'products',
+
     data() {
-        return {
+        return {productFields: [
+        {key: 'name', label: 'Nimi'},
+       {key:'category', label: 'Kategooria'}, 
+       {key:'weight', label: 'Kaal(g)'},
+    
+        {key: 'kgprice', label:'KG hind'},
+        {key:'price', label: 'Hind'},
+        ],    
+      productItems: [] ,
 
         }
     },
     async created () {
-
-
-    },
+    const products = await axios({
+      url: 'api/products',
+      method: 'GET',
+      headers: {}
+    })
+    console.log('products', products)
+    this.productItems = products.data.allProducts
+  },
     methods: {
   }
     }
 </script>
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
